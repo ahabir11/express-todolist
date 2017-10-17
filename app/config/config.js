66dc5316da18527685943efaa630d5ea1fc746cf
Akther <akther.hussain@chefonline.co.uk>
2017-10-17 13:57:58 +0600

    config file connection modification

--- app/config/config.js
+++ app/config/config.js
@@ -5,5 +5,6 @@ var config = {
     test_port: 2001,
     //test_db: "mongodb://localhost/todoapi_test"	
     test_db: "mongodb://akther:123456@ds115045.mlab.com:15045/express-todolist"
+
 }
 module.exports = config;
